455567f24b327182c8aad0e8623891cd9395d8cf
Branislav Totic <branislav.totic@gmail.com>
2018-09-10 21:40:27 +0200

    created PersonForm with 2 paramets(name,age)

---
+++ validatingFormInput/src/main/java/hello/PersonForm.java
@@ -0,0 +1,37 @@
+package hello;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class PersonForm {
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @NotNull
+    @Min(18)
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person(Name: " + this.name + " Age: " + this.age + ")";
+    }
+}
