37413458d31d41810daa8ac3c10c5844cb4549b7
Branislav Totic <branislav.totic@gmail.com>
2018-09-17 18:59:52 +0200

    Update runnble Application class

--- uploading-files/src/main/java/com/btotic/sg/uploadingfiles/Application.java
+++ uploading-files/src/main/java/com/btotic/sg/uploadingfiles/Application.java
@@ -1,14 +1,27 @@
 package com.btotic.sg.uploadingfiles;
 
+import com.btotic.sg.uploadingfiles.storage.StorageProperties;
+import com.btotic.sg.uploadingfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
     public static void main(String[] args){
 
         SpringApplication.run(Application.class, args);
 
+    }
+    @Bean
+    CommandLineRunner init(StorageService storageService){
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
